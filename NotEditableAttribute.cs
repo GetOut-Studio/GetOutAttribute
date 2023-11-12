2429bd7d848cff6909524ecce42d6a0e433913e6
TomasGranda <34276795+TomasGranda@users.noreply.github.com>
2023-11-12 18:15:15 -0300

    Reducido el atributo NotEditable

--- NotEditableAttribute.cs
+++ NotEditableAttribute.cs
@@ -2,32 +2,20 @@ using UnityEngine;
 using System;
 using UnityEditor;
 
-[AttributeUsage(AttributeTargets.Field)]
-public class NotEditableAttribute : PropertyAttribute
+namespace GetOutStudio
 {
-    public bool HideInInspector = false;
+    [AttributeUsage(AttributeTargets.Field)]
+    public class NotEditableAttribute : PropertyAttribute { }
 
-    public void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(NotEditableAttribute))]
+    public class NotEditableDrawer : PropertyDrawer
     {
-        // Saving previous GUI enabled value
-        var previousGUIState = GUI.enabled;
-        // Disabling edit for property
-        GUI.enabled = false;
-        // Drawing Property
-        EditorGUI.PropertyField(position, property, label);
-        // Setting old GUI enabled value
-        GUI.enabled = previousGUIState;
-    }
-}
-
-[CustomPropertyDrawer(typeof(NotEditableAttribute))]
-public class NotEditableDrawer : PropertyDrawer
-{
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        var previousGUIState = GUI.enabled;
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label);
-        GUI.enabled = previousGUIState;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var previousGUIState = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = previousGUIState;
+        }
     }
 }
